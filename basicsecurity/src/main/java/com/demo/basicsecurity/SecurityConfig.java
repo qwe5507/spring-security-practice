1a24a157b7dde4320e099740ff03faddeffda494
qwe5507 <apvmffkdls@gmail.com>
2023-08-09 20:02:38 +0900

    3) Form Login 인증

--- basicsecurity/src/main/java/com/demo/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/com/demo/basicsecurity/SecurityConfig.java
@@ -3,7 +3,16 @@ package com.demo.basicsecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
@@ -13,7 +22,29 @@ public class SecurityConfig {
         http.authorizeRequests()
                 .anyRequest().authenticated();
 
-        http.formLogin();
+        http.formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/loginPage")
+                .usernameParameter("userId") // default는 usernnme
+                .passwordParameter("passwd") // default는 password
+                .loginProcessingUrl("/login_proc")// default는 login
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication : " + authentication.getName());
+                        response.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        System.out.println("exception : " + exception.getMessage());
+                        response.sendRedirect("/loginPage");
+                    }
+                })
+                .permitAll(); // 로그인 페이지는 허용
+
 
         return http.build();
     }
