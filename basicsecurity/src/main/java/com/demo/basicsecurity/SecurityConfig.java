ad2eea7ea50529bb3083af4fc3ff0cd854afd450
qwe5507 <apvmffkdls@gmail.com>
2023-08-14 10:30:45 +0900

    11) 권한설정과 표현식

--- basicsecurity/src/main/java/com/demo/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/com/demo/basicsecurity/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.demo.basicsecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -10,20 +14,46 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public UserDetailsManager users() {
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+
+        UserDetails sys = User.builder()
+                .username("sys")
+                .password("{noop}1111")
+                .roles("SYS")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1111")
+                .roles("ADMIN", "SYS", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, sys, admin);
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated();
+//        http.authorizeRequests()
+//                .antMatchers("/user").hasRole("USER")
+//                .antMatchers("/admin/pay").hasRole("ADMIN")
+//                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+//                .anyRequest().authenticated();
+
+        // 스프링 시큐리티 5.4에 맞춘 강의 예제
+        http.authorizeHttpRequests(authorizeHttpRequests ->
+                authorizeHttpRequests
+                        .antMatchers("/user").hasRole("USER")
+                        .antMatchers("/admin/pay").hasRole("ADMIN")
+                        .antMatchers("/admin/**").hasAnyRole("ADMIN", "SYS")
+                        .anyRequest().authenticated());
 
         http.formLogin();
-
-        http.sessionManagement()
-                .sessionFixation().changeSessionId()// 기본값
-                                         // none, migrateSession, newSession
-//                .invalidSessionUrl("/invalid")      // 세션이 유효하지 않을 때 이동 할 페이지
-                .maximumSessions(1)                 // 최대 허용 가능 세션 수 , -1 : 무제한 로그인 세션 허용
-                .maxSessionsPreventsLogin(true)    // 동시 로그인 차단함,  false : 기존 세션 만료(default)
-                .expiredUrl("/expired");            // 세션이 만료된 경우 이동 할 페이지
-
 
         return http.build();
     }
