e96e005b41d5428053eb0d6dba7f7faed31dc377
qwe5507 <apvmffkdls@gmail.com>
2023-08-23 06:11:30 +0900

    4) 인증 저장소 - SecurityContextHolder, SecurityContext

---
+++ basicsecurity/src/main/java/com/demo/basicsecurity/SecurityConfig3.java
@@ -0,0 +1,26 @@
+package com.demo.basicsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig3 {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 스프링 시큐리티 5.4에 맞춘 강의 예제
+        http.authorizeHttpRequests(authorizeHttpRequests ->
+                authorizeHttpRequests
+                        .anyRequest().authenticated());
+
+        http.formLogin();
+
+        // 상위스레드와 하위스레드간 시큐리티 컨텍스트 공유
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+
+        return http.build();
+    }
+}
--- basicsecurity/src/main/java/com/demo/basicsecurity/SecurityController.java
+++ basicsecurity/src/main/java/com/demo/basicsecurity/SecurityController.java
@@ -1,14 +1,43 @@
 package com.demo.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+
+        Authentication authentication1 = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);//SPRING_SECURITY_CONTEXT
+        Authentication authentication2 = context.getAuthentication();
+        // authentication1 == authentication2
+
         return "home";
+    }
+
+    @GetMapping("/thread")
+    public String thread() {
+        Authentication authentication1 = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication1);
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Authentication authentication2 = SecurityContextHolder.getContext().getAuthentication();
+                        System.out.println(authentication2);
+                        //SecurityContextHolder.MODE_INHERITABLETHREADLOCAL 옵션이 아니라면 NULL
+                    }
+                }
+        ).start();
+        return "thread";
     }
 
     @GetMapping("/user")
