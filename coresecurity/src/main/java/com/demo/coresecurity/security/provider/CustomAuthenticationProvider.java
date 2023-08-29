aa83876b893bb5da7b6c999b1ff327b6ec517922
qwe5507 <apvmffkdls@gmail.com>
2023-08-30 06:00:40 +0900

    5) DB 연동 인증 처리(2) : CustomAuthenticationProvider

---
+++ coresecurity/src/main/java/com/demo/coresecurity/security/provider/CustomAuthenticationProvider.java
@@ -0,0 +1,40 @@
+package com.demo.coresecurity.security.provider;
+
+import com.demo.coresecurity.security.sevice.AccountContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = (String)authentication.getCredentials();
+
+        AccountContext accountContext = (AccountContext)userDetailsService.loadUserByUsername(username);
+
+        if (!passwordEncoder.matches(password, accountContext.getPassword())) {
+            throw new BadCredentialsException("BadCredentialsException");
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
+
+        return authenticationToken;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
