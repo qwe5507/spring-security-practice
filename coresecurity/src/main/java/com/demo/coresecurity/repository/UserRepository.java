6a29b6b191eb6f93a2d08c1733154102d6be5c3c
qwe5507 <apvmffkdls@gmail.com>
2023-08-29 22:38:25 +0900

    4) DB 연동 인증 처리(1) : CustomUserDetailsService

--- coresecurity/src/main/java/com/demo/coresecurity/repository/UserRepository.java
+++ coresecurity/src/main/java/com/demo/coresecurity/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.demo.coresecurity.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Account, Long> {
-    
+
+    Account findByUsername(String username);
 }
--- coresecurity/src/main/java/com/demo/coresecurity/security/configs/SecurityConfig.java
+++ coresecurity/src/main/java/com/demo/coresecurity/security/configs/SecurityConfig.java
@@ -1,20 +1,45 @@
 package com.demo.coresecurity.security.configs;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+
+    //    @Bean
+//    public UserDetailsManager users() {
+//
+//        String password = passwordEncoder().encode("1111");
+//
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password(password)
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails manager = User.builder()
+//                .username("manager")
+//                .password(password)
+//                .roles("MANAGER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(password)
+//                .roles("ADMIN", "USER", "MANAGER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, manager, admin);
+//    }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -28,41 +53,16 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsManager users() {
-
-        String password = passwordEncoder().encode("1111");
-
-        UserDetails user = User.builder()
-                .username("user")
-                .password(password)
-                .roles("USER")
-                .build();
-
-        UserDetails manager = User.builder()
-                .username("manager")
-                .password(password)
-                .roles("MANAGER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(password)
-                .roles("ADMIN", "USER", "MANAGER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, manager, admin);
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/", "user/login/**", "/users").permitAll()
-            .antMatchers("/mypage").hasRole("USER")
-            .antMatchers("/messages").hasRole("MANAGER")
-            .antMatchers("/config").hasRole("ADMIN")
-            .anyRequest().authenticated();
+                .antMatchers("/", "user/login/**", "/users").permitAll()
+                .antMatchers("/mypage").hasRole("USER")
+                .antMatchers("/messages").hasRole("MANAGER")
+                .antMatchers("/config").hasRole("ADMIN")
+                .anyRequest().authenticated();
 
         http.formLogin();
+
 
         return http.build();
     }
